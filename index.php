85de5670a0648b3c0dc176c783e3d2e95021f8fe
Vitor Hugo da Silva Lima <silvalimavitorhugo@gmail.com>
2020-05-04 17:09:47 -0500

    Fixing post page

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	
 	<section class="container blog-post">
         <div class="post">
             <img class="post-image" src="<?php the_field('imagem_postagem'); ?>" alt="<?php the_title(); ?>">
@@ -9,26 +8,21 @@
                 <h1 class="post-title"><?php the_title(); ?></h1>
                 <div class="post-description">
                     <p class="post-autor">Escrito por: <?php the_field('autor_postagem'); ?></p>
-                    <div class="grid-16"> 
-                    	<?php the_field('texto_postagem'); ?>
-
-                    </div>
                     <ul class="tags post-tags">
                     	<?php if(have_rows('tags_postagem')): while(have_rows('tags_postagem')) : the_row(); ?>
                         <li class="tag post-tag"><?php the_sub_field('tags'); ?></li>
                         <?php endwhile; else : endif; ?>
-                       </ul>
+                    </ul>
                 </div>
-                
-                
+
+                <div class="texto"> 
+                    <?php the_field('texto_postagem'); ?>
+                </div>
             </div>
         </div>
     </section>
 <?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+    <p><?php _e('Desculpa, nenhum post corresponde aos seus critérios.'); ?></p>
 <?php endif; ?>
-
-
-
 
 <?php get_footer(); ?>
